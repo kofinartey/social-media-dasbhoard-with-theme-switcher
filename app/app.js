11677e2dba6520acc54b39305e16a64514890e31
Kofi Nartey <eugeneahulu15@live.com>
2021-05-15 21:25:31 +0000

    Added functionality for theme toggler

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
-let themeToggler = document.querySelector(".theme__button");
+const themeToggler = document.querySelector(".theme__button");
 let buttonBkg = document.querySelector(".btn__background");
 let themeKnob = document.querySelector(".btn__knob");
 
 themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
   themeKnob.classList.toggle("btn__knob--dark");
   buttonBkg.classList.toggle("btn__background--dark");
 });
